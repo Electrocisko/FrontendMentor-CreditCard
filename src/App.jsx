d93c9ab8cfe0cfd87fe749414b5284c249b35f32
Francisco Zuidwijk <electrocisko@gmail.com>
2023-12-27 18:34:58 -0300

    working on back card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Form from "./components/form/Form";
 import Front from "./components/front/Front";
+import Back from "./components/back/Back";
 
 function App() {
   return (
     <div>
+      <Back/>
       <Front />
       <Form />
     </div>
---
+++ src/components/back/Back.jsx
@@ -0,0 +1,11 @@
+import styles from './back.module.scss'
+
+function Back() {
+  return (
+    <div className={styles.backContainer}>
+        <p>000</p>
+    </div>
+  )
+}
+
+export default Back
