a4858d9202ea2d5b81d1cb2d91acfb4cb65154f9
Francisco Zuidwijk <electrocisko@gmail.com>
2024-01-05 14:42:54 -0300

    validations

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,13 @@ function App() {
 
   function validateData(e) {
     e.preventDefault();
+    let nameOk = false;
+    let numberOk = false;
+    let monthOk = false;
+    let yearOk = false;
+    let cvcOk = false;
+    let expiredOk = false;
+
     const { name, number, month, year, cvc } = dataCard;
     setMessages({
       name: "",
@@ -39,60 +46,75 @@ function App() {
       year: "",
       cvc: "",
     });
+    setItsOk(false);
+    setSubmited(false);
 
-    name.length == 0 &&
+    if (name == "") {
       setMessages((prevState) => ({ ...prevState, name: "Can't be blank" }));
+    } else {
+      nameOk = true;
+    }
 
-    number.length != 16 &&
-      number.length > 0 &&
+    if (number.length != 16 && number != "") {
       setMessages((prevState) => ({
         ...prevState,
-        number: "must have 16 numeric characters",
+        number: "Must have 16 numeric characters",
       }));
-    number.length == 0 &&
+    } else if (number == "") {
       setMessages((prevState) => ({ ...prevState, number: "Can't be blank" }));
+    } else {
+      numberOk = true;
+    }
 
-    month.length != 2 &&
-      month.length > 0 &&
+    if (month.length != 2 && month != "") {
       setMessages((prevState) => ({
         ...prevState,
-        month: "must have 2 numeric characters",
+        month: "Must have 2 numeric characters",
       }));
-    month.length == 0 &&
+    } else if (month == "") {
       setMessages((prevState) => ({ ...prevState, month: "Can't be blank" }));
+    } else {
+      monthOk = true;
+    }
 
-    cvc.length != 3 &&
-      cvc.length > 0 &&
+    if (year.length != 2 && year != "") {
       setMessages((prevState) => ({
         ...prevState,
-        cvc: "must have 3 numeric characters",
+        year: "Must have 2 numeric characters",
       }));
-    cvc.length == 0 &&
-      setMessages((prevState) => ({ ...prevState, cvc: "Can't be blank" }));
-
-      let cardDate = new Date(parseInt(year)+2000,month-1 );
-      let currentDate = new Date();
-      if (cardDate.getFullYear() < currentDate.getFullYear()) {
-        setMessages((prevState) => ({ ...prevState, year: "Card Expired" }));
-      }
-
-      if ((cardDate.getFullYear() == currentDate.getFullYear()) && cardDate.getMonth() < currentDate.getMonth()  ) {
-        setMessages((prevState) => ({ ...prevState, year: "Card Expired" }));
-      }
-
-      year.length != 2 &&
-      year.length > 0 &&
-      setMessages((prevState) => ({
-        ...prevState,
-        year: "must have 2 numeric characters",
-      }));
-    year.length == 0 &&
+    } else if (year == "") {
       setMessages((prevState) => ({ ...prevState, year: "Can't be blank" }));
+    } else {
+      yearOk = true;
+    }
 
+    if (cvc.length != 3 && cvc != "") {
+      setMessages((prevState) => ({
+        ...prevState,
+        cvc: "Must have 2 numeric characters",
+      }));
+    } else if (cvc == "") {
+      setMessages((prevState) => ({ ...prevState, cvc: "Can't be blank" }));
+    } else {
+      cvcOk = true;
+    }
 
-    setSubmited(true);
-    if (name && number && month && year && cvc) {
+    let cardDate = new Date(parseInt(year) + 2000, month - 1);
+    let currentDate = new Date();
+    if (cardDate.getFullYear() < currentDate.getFullYear()) {
+      setMessages((prevState) => ({ ...prevState, year: "Card Expired" }));
+    } else if (
+      cardDate.getFullYear() == currentDate.getFullYear() &&
+      cardDate.getMonth() < currentDate.getMonth()
+    ) {
+      setMessages((prevState) => ({ ...prevState, year: "Card Expired" }));
+    } else {
+      expiredOk = true;
+    }
+
+    if (nameOk && numberOk && monthOk && yearOk && cvcOk && expiredOk) {
       setItsOk(true);
+      setSubmited(true);
     }
   }
 
